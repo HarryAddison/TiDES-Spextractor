7a5740501c37f42abdb8ccec55fbd703252967f5
Addison <ha00871@astro1.eps.surrey.ac.uk>
2025-09-03 13:53:41 +0100

    Removed units as they are already propagated

--- tides_spextractor/physics/line_velocity.py
+++ tides_spextractor/physics/line_velocity.py
@@ -2,7 +2,7 @@
 Author: Harry Addison
 Created: 02/06/25
 '''
-
+import astropy.units as u
 import numpy as np
 import torch
 from tides_spextractor.physics.doppler import calc_doppler_vel, calc_doppler_vel_err
@@ -20,7 +20,6 @@ def calc_vel(data, rest_wl, model, keys=["x", "y", "y_err"], **kwargs):
 
     vel = calc_doppler_vel(min_wl, rest_wl)
     vel_err = calc_doppler_vel_err(min_wl, rest_wl, min_wl_err)
-
     return vel, vel_err
 
 
@@ -33,5 +32,5 @@ def _estimate_min_wl_err(data, model, keys=["x", "y", "y_err"], n_samples=1000, 
         samples = samples.detach().cpu().numpy()
 
     min_samples_ind = np.argmin(samples, axis=1)
-    min_wl_err = np.std(data[keys[0]][min_samples_ind]) * data[keys[0]].unit
+    min_wl_err = np.std(data[keys[0]][min_samples_ind])
     return min_wl_err
