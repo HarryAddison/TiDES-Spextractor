dc82e225317d2b224978afb7ff9d9205a68b824c
Addison <ha00871@astro1.eps.surrey.ac.uk>
2025-09-03 13:51:26 +0100

    binning

--- tides_spextractor/maths/interpolation.py
+++ tides_spextractor/maths/interpolation.py
@@ -28,7 +28,7 @@ def interpolate_linear(x_data, y_data, x_new):
     return y_new
 
 
-def downsample_spec_data(data, ds_factor, keys=["x", "y", "y_err"]):
+def downsample_spec_data(data, ds_factor, keys=["x", "y", "y_err"], **kwargs):
 
     n_bins = int(len(data[keys[0]]) / ds_factor)
     bin_edges= np.linspace(data[keys[0]].min(), data[keys[0]].max(),
@@ -56,3 +56,34 @@ def downsample_spec_data(data, ds_factor, keys=["x", "y", "y_err"]):
                                       bin_data[keys[2]][0]])
 
     return downsampled_data
+
+
+def convolveTo15A(x,nBins=60):
+    return np.convolve(x, np.ones(nBins), 'valid') / nBins
+
+
+def convolveNoiseTo15A(x,nBins=60):
+    for m in range(len(x)-(nBins-1)):
+        yield np.sqrt(sum((np.ones(nBins) * x[m:m+nBins])**2)) / nBins
+
+
+def bin_spec_data(data, bin_width, keys=["x", "y", "y_err"], **kwargs):
+
+    # convert data to dimensionless quantities
+    wave = data[keys[0]].value
+    signal = data[keys[1]].value
+    noise = data[keys[2]].value
+
+    n_bins = int((max(wave) - min(wave)) / bin_width) + 1
+
+    if n_bins != None:
+        # rebin the data
+        # Convert fluxes to flux * wave before binning then convert back.
+        wave15 = convolveTo15A(wave, n_bins)
+        signal = convolveTo15A(signal * wave, n_bins) / wave15
+        noise = np.array(list(convolveNoiseTo15A(noise * wave, n_bins))) / wave15
+
+    data_binned = QTable(data=[wave15 * data[keys[0]].unit,
+                               signal * data[keys[1]].unit,
+                               noise * data[keys[2]].unit], names=keys)
+    return data_binned
