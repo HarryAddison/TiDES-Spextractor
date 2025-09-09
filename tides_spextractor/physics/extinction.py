c2f09fb2497442108a0db998640df14f515469f1
Addison <ha00871@cluster1.ph.surrey.ac.uk>
2025-09-09 17:03:40 +0100

    Updating to use keys argument instead of hardcoded keys. Added units to output of get_mwebv

--- tides_spextractor/physics/extinction.py
+++ tides_spextractor/physics/extinction.py
@@ -3,23 +3,24 @@ Author: Harry Addison
 Created: 17/02/2025
 '''
 
+import astropy.units as u
 import numpy as np
 from astropy.coordinates import SkyCoord
 from dustmaps.sfd import SFDQuery
 from extinction import fitzpatrick99, remove
 
 
-def deredden(data, ebv, rv):
-    if data["wave"].unit in ["Angstrom", "aa", "A"]:
+def deredden(data, ebv, rv, keys=["x", "y", "y_err"], **kwargs):
+    if data[keys[0]].unit in ["Angstrom", "aa", "A"]:
         unit = "aa"
     else:
         raise ValueError("Wavelength unit should be in angstroms")
     # wavelength dtype has to be float64 otherwise fitzpatrick99 errors. 
-    wave = np.array(data["wave"].value, dtype=np.float64)
+    wave = np.array(data[keys[0]].value, dtype=np.float64)
     wl_dependent_ebv = fitzpatrick99(wave, a_v=(rv * ebv.value),
                                      r_v=rv, unit=unit)
 
-    data["flux"] = remove(wl_dependent_ebv, data["flux"])
+    data[keys[1]] = remove(wl_dependent_ebv, data[keys[1]])
 
     return data
 
@@ -30,6 +31,6 @@ def get_mwebv(ra, dec, frame='icrs', **kwargs):
     coords for the MW Galaxy extinction ebv.
     '''
     coord = SkyCoord(ra, dec, frame=frame)
-    mwebv = float(SFDQuery()(coord))
+    mwebv = float(SFDQuery()(coord)) * u.mag
 
     return mwebv
